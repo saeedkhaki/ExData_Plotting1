24998b0a14246e3613b2acb088cc8eb0c8ae85d3
saeedkhaki <ie.saeed.khaki@gmail.com>
2014-08-10 04:58:28 -0700

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,3 +1,9 @@
+data = read.table("saeed.txt", sep=";")
+
+#i seprated it first manaully sorry... i had not had much time only 1 hour 
+
+data$V3<- as.numeric(data$V3)
+
 ts1<- ts(data$V3,frequency=365)
 
 plot(ts1,ylab="Active Global power(killowat)")  
