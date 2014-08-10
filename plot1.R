b2211bf28a4a7408f11f8dead4057035bca450fa
saeedkhaki <ie.saeed.khaki@gmail.com>
2014-08-10 04:57:04 -0700

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,3 +1,9 @@
+data = read.table("saeed.txt", sep=";")
+
+#i seprated it first manaully sorry... i had not had much time only 1 hour 
+
+data$V3<- as.numeric(data$V3)
+
 data$V3<- as.numeric(data$V3)
 
 hist(data$V3,col="red",breaks=8,main="Global Active power",xlab="Active Global power(killowat)")
