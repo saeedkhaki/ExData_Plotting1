ee35f4fa4a5108c2d65e86a47b13e80ad5acbd57
saeedkhaki <ie.saeed.khaki@gmail.com>
2014-08-10 11:18:05 -0700

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,12 +1,31 @@
-data = read.table("saeed.txt", sep=";")
+data <- read.table("household_power_consumption.txt", sep=";")
 
-#i seprated it first manaully sorry... i had not had much time only 1 hour 
+data<-data[-1,]   #omiting first row (because this was name)
 
-data$V3<- as.numeric(data$V3)
+#changing columns name to the default
 
-data$V3<- as.numeric(data$V3)
+colnames(data) <- c("Date"," Time"," Global_active_power", "Global_reactive_power", "Voltage" ,"Global_intensity", "Sub_metering_1", "Sub_metering_2","Sub_metering_3")
 
-hist(data$V3,col="red",breaks=8,main="Global Active power",xlab="Active Global power(killowat)")
+sub1<-subset(data,Date=="1/2/2007")
+
+sub2<-subset(data,Date=="2/2/2007")
+#this is the final data set
+sub3<-rbind(sub1,sub2)
+
+sub3[,3]<-as.numeric(as.character(sub3[,3]))
+
+hist(sub3[,3],col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
 
 dev.copy(png,file="plot 1")
 dev.off()
+
+
+
+
+
+
+
+
+
+
+
